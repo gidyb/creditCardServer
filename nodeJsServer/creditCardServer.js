c6558f0000348c5d0b36d6e1ad56a0cf317bdeaf
Gidy Basin <gidyb@users.noreply.github.com>
2015-05-16 10:51:04 +0300

    Basic exercise + 1st bonus (async storage)

--- nodeJsServer/creditCardServer.js
+++ nodeJsServer/creditCardServer.js
@@ -1,9 +1,20 @@
+// Required Modules
 var express = require('express');
 var uuid = require('uuid');
+var redis = require('redis');
+var async = require('async');
 
 // Credit cards data
 var storage = {};
 
+// Create redis client
+var redisClient = redis.createClient();
+
+redisClient.on("error", function (err) {
+	console.log(err);
+});
+
+// Create Express web server
 var app = express();
 
 app.post('/creditcard', function(request,response) {
@@ -24,39 +35,54 @@ app.post('/creditcard', function(request,response) {
 
 		console.log("Request to save card with number = " + cardNumber);
 
-		// Check if card already exists
 		var cardFound = false;
 
-		var cardIds = Object.keys(storage);
-		cardIds.forEach(function (id){
-			if (storage[id] == cardNumber) {
-				cardFound = true;
-			}
+		// Check if card already exists
+		redisClient.keys('*', function (err, keys) {
+
+			if (err) return console.log(err);
+
+			async.each(keys,
+
+				// For each key
+				function(key, callback){
+					redisClient.get(key, function (err, reply) {
+						if (reply == cardNumber) {
+							cardFound = true;
+						}
+						// Finished with this key
+						callback();
+					});
+				},
+
+				// When all keys are checked
+				function(){
+					if (cardFound) {
+						// It's a known credit card
+						console.log("Card already exists");
+						response.write("This card already exists!");
+						response.end();
+					}
+					else{
+						// It's a new credit card
+
+						// Compute a UUID for the card
+						var cardId = uuid.v4();
+
+						// Save card
+						console.log("Saving card with id = " + cardId);
+						redisClient.set(cardId,cardNumber);
+
+						// Build response
+						var responseJson = {};
+						responseJson.token = cardId;
+						response.write(JSON.stringify(responseJson));
+					}
+
+					response.end();
+				});
 		});
-
-		if (cardFound){
-			// It's a known credit card
-			console.log("Card already exists");
-			response.write("This card already exists!");
-		}
-		else{
-			// It's a new credit card
-
-			// Compute a UUID for the card
-			var cardId = uuid.v4();
-
-			// Save card
-			console.log("Saving card with id = " + cardId);
-			storage[cardId] = cardNumber;
-
-			// Build response
-			var responseJson = {};
-			responseJson.token = cardId;
-			response.write(JSON.stringify(responseJson));
-		}
-
-		response.end();
-	})
+	});
 });
 
 app.get('/creditcard/:id', function(request,response){
@@ -67,24 +93,27 @@ app.get('/creditcard/:id', function(request,response){
 
 	response.writeHead(200, {'Content-Type': 'application/json'});
 
-	if (storage.hasOwnProperty(cardId)) {
-		// Card exists
+	redisClient.exists(cardId, function(err,reply){
+		if (reply == 1){
+			// Card exists
+			redisClient.get(cardId, function(err, reply){
+				var cardNumber = reply;
+				console.log("Card found. CardNumber = " + cardNumber);
 
-		var cardNumber = storage[cardId];
-		console.log("Card found. CardNumber = " + cardNumber);
-
-		// Build response
-		var responseJson = {};
-		responseJson["credit-card"] = cardNumber;
-		response.write(JSON.stringify(responseJson));
-	}
-	else{
-		// Card does not exist
-		console.log("Card was not found");
-		response.write("There is no card with this id!");
-	}
-
-	response.end();
+				// Build response
+				var responseJson = {};
+				responseJson["credit-card"] = cardNumber;
+				response.write(JSON.stringify(responseJson));
+				response.end();
+			});
+		}
+		else{
+			// Card does not exist
+			console.log("Card was not found");
+			response.write("There is no card with this id!");
+			response.end();
+		}
+	});
 });
 
 app.listen(8081);
